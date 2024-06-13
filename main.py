44c6fb2dea3ab8557881502671e7574782cbbc98
sdolphen <stijndolphen@gmail.com>
2024-06-13 21:52:54 +0200

    dbgg

--- main.py
+++ main.py
@@ -55,12 +55,7 @@ fig.update_traces(
 # Display the bar chart in the Streamlit app
 st.plotly_chart(fig, use_container_width=True)
 
-# Add the tweet below the bar chart
+# Add the tweet below the bar chart using iframe with the correct URL format
 tweet_url = "https://x.com/EURO2024/status/1801255704197533986"  # Replace with your tweet URL
-tweet_html = f'''
-<blockquote class="twitter-tweet">
-    <a href="{tweet_url}"></a>
-</blockquote>
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-'''
-components.html(tweet_html, height=600)
+tweet_embed_url = f"https://twitframe.com/show?url={tweet_url}"
+components.iframe(tweet_embed_url, height=600)
